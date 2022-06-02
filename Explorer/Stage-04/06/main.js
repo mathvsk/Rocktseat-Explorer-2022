88e44cd4d8aa74adcaec9ffe7e4c76073bf1652f
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-06-01 23:29:16 -0300

    Jogo da adivinhação (while)
    adiciona uma validação pra exibir uma mensagem, caso o player acerte na primeira tentativa

--- Explorer/Stage-04/06/main.js
+++ Explorer/Stage-04/06/main.js
@@ -1,10 +1,15 @@
-let message = Number(prompt("Advinhe o número que estou pensando? Está entre 0 e 10"))
 const randomNumber = Math.round(Math.random() * 10)
+console.log(randomNumber)
+let message = Number(prompt("Advinhe o número que estou pensando? Está entre 0 e 10"))
 
 let attempts = 1
-while (message != randomNumber) {
-    tentativas ++
-    message = Number(prompt("Advinhe o número que estou pensando? Está entre 0 e 10"))
+5
+if (message == randomNumber && attempts == 1)  {
+    alert(`Voce acertou em ${attempts} tentativa. O número que eu estava pensando era ${randomNumber}`)
+} else { 
+    while (message != randomNumber) {
+        attempts++
+        message = Number(prompt("Advinhe o número que estou pensando? Está entre 0 e 10"))
+    }
+    alert(`Voce acertou em ${attempts} tentativas. O número que eu estava pensando era ${randomNumber}`)
 }
-
-alert(`Voce acertou em ${tentativas} tentativas. O número que eu estava pensando era ${randomNumber}`)
