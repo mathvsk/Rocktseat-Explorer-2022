744fa4c24f6914aa502afc1c0aace5dcf11bc113
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-06-06 20:19:07 -0300

    Refatorando para novas funcionalidades

--- Explorer/Stage-05/script.js
+++ Explorer/Stage-05/script.js
@@ -1,14 +1,44 @@
-let play = document.querySelector('.play')
-let pause = document.querySelector('.pause')
+// DOM
+// Document Object Model
+
+// refatoração: mudar um código para deixá-lo mais entendível
+// deixar o código mais performático
+// SEM ALTERAR suas funcionalidades
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+
 // Event-driven
 // programação imperativa
 // callback
-play.addEventListener('click', function() {
-  play.classList.add('hide')
-  pause.classList.remove('hide')
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 })
 
-pause.addEventListener('click', function() {
-  pause.classList.add('hide')
-  play.classList.remove('hide')
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
 })
