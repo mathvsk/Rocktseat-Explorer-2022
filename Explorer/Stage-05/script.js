4e2549a65bd23e0fa20c85834ab1433233f49db2
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-06-06 20:38:59 -0300

    Avançando na lógica com pequenos passos
    versão bugada

--- Explorer/Stage-05/script.js
+++ Explorer/Stage-05/script.js
@@ -15,19 +15,33 @@ const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
 function countdown(){
-  setTimeout(function() {
-    let seconds =  Number(secondsDisplay.textContent)
-
-    if( seconds <= 0 ) {
-      seconds = 60
-    }
-
-    secondsDisplay.textContent = seconds - 1
+    setTimeout(function() {
+      let seconds =  Number(secondsDisplay.textContent)
+      let minutes = Number(minutesDisplay.textContent)
+  
+      secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+  
+      if (minutes <= 0) {
     
-    // recursão: quando uma função chama ela mesma
-    countdown()
-  }, 1000)
-}
+        buttonPlay.classList.remove('hide')
+        buttonPause.classList.add('hide')
+        buttonSet.classList.remove('hide')
+        buttonStop.classList.add('hide')
+  
+        return
+      }
+  
+      if( seconds <= 0 ) {
+        seconds = 60
+  
+        minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+      }
+  
+      secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+      
+      countdown()
+    }, 1000)
+  }
 
 // Event-driven
 // programação imperativa
@@ -65,5 +79,5 @@ buttonSoundOn.addEventListener('click', function() {
 
 buttonSet.addEventListener('click', function() {
     minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = minutes
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
   })
