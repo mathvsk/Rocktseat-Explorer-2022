60221a240940fe770679193822c1222fdafadff6
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-06-06 22:04:47 -0300

    Introdução aos ES6 Modules
    começando a separar os arquivos

--- Explorer/Stage-05/js/index.js
+++ Explorer/Stage-05/js/index.js
@@ -1,3 +1,6 @@
+import resetControls from "./controls.js"
+import "./timer.js"
+
 // DOM
 // Document Object Model
 
@@ -14,45 +17,6 @@ const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
-
-function resetControls() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
-}
-
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function resetTimer() {
-    updateTimerDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
-}
-
-function countdown() {
-    timerTimeOut = setTimeout(function () {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        updateTimerDisplay(minutes, 0)
-
-        if (minutes < 0) {
-            resetControls()
-            return
-        }
-
-        if (seconds <= 0) {
-            seconds = 60
-            --minutes
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1))
-        countdown()
-    }, 1000)
-}
 
 // Event-driven
 // programação imperativa
