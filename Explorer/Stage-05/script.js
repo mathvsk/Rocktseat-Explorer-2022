0f27da82b583b4524ca9eea6848b643d9bc5dcd6
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-06-06 21:02:27 -0300

    Princípios de Clean Code e programação declarativa

--- Explorer/Stage-05/script.js
+++ Explorer/Stage-05/script.js
@@ -14,70 +14,71 @@ let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
-function countdown(){
-    setTimeout(function() {
-      let seconds =  Number(secondsDisplay.textContent)
-      let minutes = Number(minutesDisplay.textContent)
-  
-      secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-  
-      if (minutes <= 0) {
-    
-        buttonPlay.classList.remove('hide')
-        buttonPause.classList.add('hide')
-        buttonSet.classList.remove('hide')
-        buttonStop.classList.add('hide')
-  
-        return
-      }
-  
-      if( seconds <= 0 ) {
-        seconds = 60
-  
-        minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
-      }
-  
-      secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
-      
-      countdown()
-    }, 1000)
-  }
+function resetControls() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+}
 
+function countdown() {
+    setTimeout(function () {
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+
+        updateTimerDisplay(minutes, 0)
+
+        if (minutes <= 0) {
+            resetControls()
+            return
+        }
+
+        if (seconds <= 0) {
+            seconds = 2
+            --minutes
+        }
+
+        updateTimerDisplay(minutes, String(seconds - 1))
+        countdown()
+    }, 1000)
+}
+
+function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
 // Event-driven
 // programação imperativa
 // callback
-buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
+buttonPlay.addEventListener('click', function () {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
 
-  countdown()
+    countdown()
 })
 
-buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
+buttonPause.addEventListener('click', function () {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
 })
 
-buttonStop.addEventListener('click', function() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+buttonStop.addEventListener('click', function () {
+    resetControls()
 })
 
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOn.classList.remove('hide')
-  buttonSoundOff.classList.add('hide')
+buttonSoundOff.addEventListener('click', function () {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
 })
 
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide')
-  buttonSoundOff.classList.remove('hide')
+buttonSoundOn.addEventListener('click', function () {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
 })
 
-buttonSet.addEventListener('click', function() {
+buttonSet.addEventListener('click', function () {
     minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-  })
+    updateTimerDisplay(minutes, 0)
+})
