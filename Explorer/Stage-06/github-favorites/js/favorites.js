f85e56c7bc4b7b5c9791596d551ab916a5be5d58
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-17 23:18:43 -0300

    Imutabilidade para trabalhar com os dados da aplicação

--- Explorer/Stage-06/github-favorites/js/favorites.js
+++ Explorer/Stage-06/github-favorites/js/favorites.js
@@ -15,6 +15,11 @@ export class Favorites {
             }
         ]
     }
+
+    delete(user) {
+        const filteredEntries = this.entries
+            .filter(entry => entry.login !== user.login)
+    }
 }
 
 // classe que vai criar a view e events do html
@@ -40,6 +45,15 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if (isOk) {
+                    user.delete(user)
+                }
+            }
+
             this.tbody.append(row)
         })
     }
