3326702e31d4dae8a6f96f95d14749292a25064b
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-19 19:58:26 -0300

    Promessas com Async Await

--- Explorer/Stage-06/github-favorites/js/favorites.js
+++ Explorer/Stage-06/github-favorites/js/favorites.js
@@ -25,6 +25,10 @@ export class Favorites {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
     }
 
+    async add(username) {
+        const user = await GithubUser.search(username)
+    }
+
     delete(user) {
         const filteredEntries = this.entries
             .filter(entry => entry.login !== user.login)
@@ -43,6 +47,16 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
