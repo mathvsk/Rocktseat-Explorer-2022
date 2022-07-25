e16006eb2db2ed934f623244d381bbd495a2b44c
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-25 20:36:03 -0300

    Controllers

---
+++ Explorer/Stage-08/API/src/routes/controllers/UsersController.js
@@ -0,0 +1,13 @@
+class UsersController {
+    /* 
+        BOAS PRÁTICAS
+        - Controller pode ter no máximo 5 [funcões ou métodos]
+            - index - GET listar vários registros.
+            - show - GET para exibir um registro específico.
+            - create - POST para criar um registro.
+            - update - PUT para atualizar um registro.
+            - delete - DELETE para remover um registro.
+    */
+};
+
+module.exports = UsersController;
