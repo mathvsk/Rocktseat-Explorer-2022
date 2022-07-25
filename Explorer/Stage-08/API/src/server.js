075f6196273ff93bdea870a14e4278dd9f5c2d84
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-25 19:18:29 -0300

    Método GET

--- Explorer/Stage-08/API/src/server.js
+++ Explorer/Stage-08/API/src/server.js
@@ -2,6 +2,10 @@ const express = require('express'); // importando o express
 
 const app = express(); // inicializando o express
 
+app.get('/', (request, response) => { // método GET
+    response.send("Hello World");
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
 
