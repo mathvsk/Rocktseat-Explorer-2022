eff43490b35940bbc5f11e8b93e0840a7977b415
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-25 19:38:20 -0300

    Query Params

--- Explorer/Stage-08/API/src/server.js
+++ Explorer/Stage-08/API/src/server.js
@@ -2,10 +2,16 @@ const express = require('express'); // importando o express
 
 const app = express(); // inicializando o express
 
-app.get('/:id/:user', (request, response) => { // método GET
+app.get('/route/:id/:user', (request, response) => { // método GET
     const { id, user } = request.params // desestruturando o ID e USER do request
 
     response.send(`ID = ${id}. USER = ${user}`)
+});
+
+app.get('/query', (request, response) => { // não é obrigatório o uso de parametro na url
+    const {idade, user } = request.query // desestruturando o IDADE e USER do query params
+
+    response.send(`ID = ${idade}. USER = ${user}`)
 });
 
 const PORT = 3333;
