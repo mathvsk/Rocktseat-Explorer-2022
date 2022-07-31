e3ee42556c69e59d29dfa788d0d93fe2d3ffe830
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-07-31 16:47:29 -0300

    Cadastrando usuário

--- Explorer/Stage-08/API/src/controllers/UsersController.js
+++ Explorer/Stage-08/API/src/controllers/UsersController.js
@@ -13,14 +13,16 @@ class UsersController {
     */
 
     async create (request, response) {
-        const { name, email, password } = request.body
+        const { name, email, password } = request.body;
 
         const database = await sqliteConnection();
-        const checkUserExists = await database.get(`SELECT * FROM users WHERE email = ${email}`)
+        const checkUserExists = await database.get(`SELECT * FROM users WHERE email = (?)`, [email])
 
         if (checkUserExists) {
             throw new AppError('Este e-mail já está em uso');
         }
+
+        await database.run(`INSERT INTO users (name, email, password) VALUES ((?), (?), (?))`, [name, email, password]);
 
         return response.status(201).json();
     }
