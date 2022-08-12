2fd95eb119eccace43f3b697aaae9f705efb52cf
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-11 21:14:02 -0300

    Atualizando senha - chamando outra função da bcryptjs para comparar a senha antiga com a nova - criando a regra de alteração de senha

--- Explorer/Stage-08/API/src/controllers/UsersController.js
+++ Explorer/Stage-08/API/src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require('../database/sqlite');
 const { request } = require("express");
@@ -31,7 +31,7 @@ class UsersController {
     }
 
     async update (request, response) {
-        const { name, email} = request.body;
+        const { name, email, password, old_password} = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();
@@ -50,13 +50,28 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        if(password && !old_password) {
+            throw new AppError('Você precisa informar a senha antiga para definir uma nova senha');
+        }
+
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(!checkOldPassword){
+                throw new AppError('A senha antiga não confere');
+            }
+
+            user.password = await hash(password, 8);
+        }
+
         await database.run(`
             UPDATE users SET
             name = ?,
             email = ?,
+            password = ?,
             update_at = ?
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, new Date(), id]
         );
 
         return response.json();
