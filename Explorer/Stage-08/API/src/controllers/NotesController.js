935c1d4707e932ac9d781877942e9d1b069fb85d
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-18 19:45:05 -0300

    Aplicando Inner Join

--- Explorer/Stage-08/API/src/controllers/NotesController.js
+++ Explorer/Stage-08/API/src/controllers/NotesController.js
@@ -65,7 +65,16 @@ class NotesController {
         const filterTags = tags.split(',').map(tag => tag.trim());
 
         notes = await knex('tags')
-            .whereIn('name', filterTags)
+        .select([
+            'notes.id',
+            'notes.title',
+            'notes.user_id'
+        ])
+        .where('notes.user_id', user_id)
+        .whereLike('notes.title', `%${title}%`)
+        .whereIn('name', filterTags)
+        .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .orderBy('notes.title')
 
     } else {
         notes = await knex('notes')
