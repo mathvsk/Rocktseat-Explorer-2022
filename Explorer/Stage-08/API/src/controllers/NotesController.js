ae07eff569fa33c8058cdbba12e36e5a59b534e3
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-17 21:35:20 -0300

    Deletando nota - Criando função para deletar nota na Controller de Nota - Criando rota em routes

--- Explorer/Stage-08/API/src/controllers/NotesController.js
+++ Explorer/Stage-08/API/src/controllers/NotesController.js
@@ -48,6 +48,13 @@ class NotesController {
     });
    }
 
+   async delete(request, response) {
+    const { id } = request.params;
+
+    await knex('notes').where({ id }).delete();
+
+    return response.json();
+   }
 }
 
 module.exports = NotesController;
