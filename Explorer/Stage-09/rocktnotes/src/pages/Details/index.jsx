44b05b180d934278e3db8850318a30ab934ffb29
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-21 19:38:42 -0300

    Finalizando Detalhes - ajustado a posição dos componentes na tela

--- Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./style"
+import { Container, Links, Content } from "./style"
 import { Button } from "../../components/button"
 import { Header } from "../../components/header"
 import { Section } from "../../components/section"
@@ -10,21 +10,37 @@ export function Details () {
     <Container>
       <Header />
 
-      <ButtonText title="Excluir Nota"/>
-    
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="node"/>
-      </Section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Voltar"/>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam omnis numquam adipisci ducimus, enim, iure neque eaque vero illum dolorem, expedita sunt quibusdam? Modi, earum minus! Adipisci modi harum vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam id eveniet, debitis sunt quia tenetur ut accusantium, ab blanditiis maxime suscipit? Consequuntur odit facere ad, laudantium sunt atque inventore molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolorem iste enim qui perspiciatis? Delectus, nesciunt accusantium commodi ipsum consequatur voluptates accusamus. Maiores autem, in itaque optio eius nulla quisquam?
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+              <li>
+                <a href="#">Link 2</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
-  )
+  );
 }
