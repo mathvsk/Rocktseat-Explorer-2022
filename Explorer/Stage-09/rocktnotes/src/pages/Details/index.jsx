4c1b0dd620413fdc344bddfd01a2bd813272da90
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-21 17:09:43 -0300

    Componente - criado componente botão

--- Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from "./style"
+import { Button } from "../../components/button"
 
 export function Details () {
 
   return (
     <Container>
       <h1>Hello World!</h1>
+
+      <Button />
     </Container>
   )
 }
