ddf88e51b14466cc0d33e689fb0aa3bb1a0c8a17
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-21 17:51:14 -0300

    Imagem do usuário - adicionado imagem de user, direto do github.

--- Explorer/Stage-09/rocktnotes/src/components/header/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/components/header/index.jsx
@@ -1,10 +1,17 @@
-import { Container } from "./style"
+import { Container, Profile } from "./style"
 
 export function Header () {
     return (
     <>
       <Container>
-        
+        <Profile>
+          <img src="https://github.com/mathvsk.png" alt="Foto de usuário" />
+
+          <div>
+            <span>Bem-vindo,</span>
+            <strong>Matheus</strong>
+          </div>
+        </Profile>
       </Container>
     </>
     )
