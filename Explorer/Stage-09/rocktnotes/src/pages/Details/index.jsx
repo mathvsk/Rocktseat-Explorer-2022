57e9b1372284c2c8e37361eb72f93752800e4056
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-21 17:20:27 -0300

    Propriedade - criado propriedade loading

--- Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/Details/index.jsx
@@ -2,12 +2,11 @@ import { Container } from "./style"
 import { Button } from "../../components/button"
 
 export function Details () {
-
   return (
     <Container>
       <h1>Hello World!</h1>
 
-      <Button />
+      <Button title="Clique aqui" loading/>
     </Container>
   )
 }
