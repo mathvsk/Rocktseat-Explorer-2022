d432e0b8058c9a36024d0b4f97aca618d1fbe6fd
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-08-25 21:14:21 -0300

    Formulário da SignIn - criado os componentes que serão usado na pág de login

--- Explorer/Stage-09/rocktnotes/src/pages/SignIn/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/SignIn/index.jsx
@@ -1,11 +1,36 @@
+import { FiMail, FiLock } from 'react-icons/fi';
 
-import { Container } from './style';
-
+import { Container, Form } from './style';
+import { Input } from '../../components/input';
+import { Button } from '../../components/button';
 
 export function SignIn() {
     return (
         <Container>
-            
+            <Form>
+                <h1>Rocket Notes</h1>
+
+                <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input 
+                    placeholder="E-mail"
+                    type="email"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button  title="Entrar" />
+
+                <a href="">Criar conta</a>
+
+            </Form>
         </Container>
     );
 }
