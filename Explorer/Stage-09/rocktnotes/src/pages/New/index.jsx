fb6f08127fb2af39caa3380b569d48b5a89f1756
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-01 20:56:47 -0300

    Header da New  - criado componente Form - importado componente Input

--- Explorer/Stage-09/rocktnotes/src/pages/New/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/New/index.jsx
@@ -1,11 +1,23 @@
 import { Header } from '../../components/header';
+import { Input } from '../../components/input';
 
-import { Container } from './style';
+import { Container, Form } from './style';
 
 export function New() {
     return (
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="">voltar</a>
+                    </header>
+
+                    <Input Placeholder="Título" />
+                </Form>
+            </main>
         </Container>
-    )
+    );
 }
