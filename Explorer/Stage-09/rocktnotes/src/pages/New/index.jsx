b3e0ab94f39e8034e36464e99f918417c0f5bfa9
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-01 20:49:00 -0300

    Interface New - criado novo componente (new)

---
+++ Explorer/Stage-09/rocktnotes/src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/header';
+
+import { Container } from './style';
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+        </Container>
+    )
+}
