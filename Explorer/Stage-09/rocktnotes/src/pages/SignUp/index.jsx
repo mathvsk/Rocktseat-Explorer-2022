7d7cdc0e512f6853795554610f3adb2a2105e159
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-20 20:55:09 -0300

    Cadastro de usuários - criado regra de negócio e validações para cadastrar o user no banco de dados

--- Explorer/Stage-09/rocktnotes/src/pages/SignUp/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
 import { FiUser, FiMail, FiLock } from "react-icons/fi";
+
+import { api } from '../../services/api'
 
 import { Container, Form, Background } from "./style";
 import { Input } from "../../components/input";
 import { Button } from "../../components/button";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export function SignUp() {
+    // criar um estado para capturar a informações e armazenar em memoria
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    function handleSignUp(){
+        if(!name || !email || !password ) {
+            return alert('Preencha todos os campos')
+        }
+
+        api.post('/user', { name, email, password })
+        .then(() => {
+             alert("Usuário cadastrado com sucesso!")
+             navigate('/')
+        })
+        .catch(error => {
+            if(error.response){ // verificando se existe um erro personalizado no nosso backend
+                alert(error.response.data.message) 
+            } else {
+                alert('Não foi possível cadastrar o usuário')
+            }
+        })
+    }
+
     return (
         <Container>
             <Background />
@@ -19,23 +48,26 @@ export function SignUp() {
 
                 <Input
                     placeholder="Nome" 
-                    title="text"
+                    type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)} // onChange(dispara evento toda vez que o input muda). Capturando o evento(e) e atribuindo para setName 
                 />
 
                 <Input 
                     placeholder="E-mail" 
-                    title="email"
+                    type="email"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                     />
 
                 <Input 
                     placeholder="Senha" 
-                    title="password"
-                    icon={FiLock}        
-                    />
+                    type="password"
+                    icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}        
+                />
 
-                <Button title="Cadastrar" />
+                <Button title="Cadastrar" onClick={handleSignUp} />
 
                 <Link to="/">Voltar para Login</Link>
             </Form>
