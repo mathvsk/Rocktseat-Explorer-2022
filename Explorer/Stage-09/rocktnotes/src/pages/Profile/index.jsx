a62d313d4a078103c2ffb0f330483d3dff3ba6c4
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-22 20:07:28 -0300

    Carregando dados do usuário - exibindo as informações do user pelo contexto, e preenchendo nos inputs

--- Explorer/Stage-09/rocktnotes/src/pages/Profile/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/Profile/index.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import { FiUser, FiMail, FiLock, FiArrowLeft, FiCamera } from "react-icons/fi";
 
 import { Container, Form, Avatar } from "./style";
 import { Input } from "../../components/input";
 import { Button } from "../../components/button";
+import { useAuth } from "../../hooks/auth";
 import { Link } from 'react-router-dom';
 
 
 export function Profile() {
+    // Pegando as informaçoes do user referente ao contexto
+    const { user } = useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [passwordOld, setpasswordOld] = useState();
+    const [passwordNew, setpasswordNew] = useState();
+
     return (
         <Container>
             <header>
@@ -34,24 +44,30 @@ export function Profile() {
                     placeholder="Nome"
                     type="text"
                     icon={FiUser}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     placeholder="E-mail"
                     type="email"
                     icon={FiMail}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Senha atual"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setpasswordOld(e.target.value)}
                 />
 
                 <Input
                     placeholder="Nova senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setpasswordNew(e.target.value)}
                 />
 
                 <Button title="Salvar" />
