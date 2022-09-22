c72def400ae87088ea09404fe9203ac7dba9bbdc
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-22 19:56:17 -0300

    Ajuste no header das requisições - utilizado a maneira mais recentente de capturar o token pelo axios

--- Explorer/Stage-09/rocktnotes/src/hooks/auth.jsx
+++ Explorer/Stage-09/rocktnotes/src/hooks/auth.jsx
@@ -13,7 +13,7 @@ function AuthProvider({ children }) {
             const response = await api.post('/sessions', {  email, password });
             const { user, token } = response.data;
             
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
             setData({ user, token });
     
             localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
@@ -41,7 +41,7 @@ function AuthProvider({ children }) {
         const user = localStorage.getItem('@rocketnotes:user');
 
         if(token && user) {
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
             setData({
                 token,
