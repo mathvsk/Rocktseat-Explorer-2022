2f60193b5651603e60777ba8632753ba6623a3cd
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-22 21:23:53 -0300

    Adicionando link na nota - usado map para adicionar item - usado useState para pegar o estado do componente - criado 2 variaveis ( uma pra manter os links, e outra para add um novo link)

--- Explorer/Stage-09/rocktnotes/src/pages/New/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/New/index.jsx
@@ -5,10 +5,20 @@ import { NoteItem } from '../../components/noteitem';
 import { Section } from '../../components/section';
 import { Button } from '../../components/button';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import { Container, Form } from './style';
 
 export function New() {
+    const [links, setlinks] = useState([]);
+    const [newLink, setNewLink] = useState("");
+
+    function handleAddLink() {
+        // pegando o estado anterior do componente
+        setlinks(prevState => [...prevState, newLink]),
+        setNewLink("");
+    }
+
     return (
         <Container>
             <Header />
@@ -25,8 +35,22 @@ export function New() {
                     <TextArea placeholder="Observações" />
 
                     <Section title="Links úteis">
-                        <NoteItem />
-                        <NoteItem placeholder="Novo link" isNew />
+                        {
+                            links.map((link, index) =>	(
+                                <NoteItem 
+                                    key={String(index)}
+                                    value={link}
+                                    onClick={() => { }}
+                                /> 
+                            ))
+                        }
+                        <NoteItem 
+                            isNew 
+                            placeholder="Novo link" 
+                            value={newLink}
+                            onChange={e => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
                     </Section>
 
                     <Section title="Marcadores">
