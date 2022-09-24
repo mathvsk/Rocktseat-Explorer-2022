c8efabf2256ae062394ca653d36d32f7871f2027
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-24 19:53:11 -0300

    Pesquisar notas - adicionado um event como parametro no onChange, para pesquisar pelo campo de texto

--- Explorer/Stage-09/rocktnotes/src/pages/Home/index.jsx
+++ Explorer/Stage-09/rocktnotes/src/pages/Home/index.jsx
@@ -20,7 +20,7 @@ export function Home() {
         if (tagName === 'all') {
             return setTagsSelected([]);
         }
-        
+
         const alreadySelected = tagsSelected.includes(tagName)
         
         if (alreadySelected) {
@@ -87,7 +87,7 @@ export function Home() {
                 <Input 
                     placeholder="Pesquisar pelo título" 
                     icon={FiSearch}
-                    onChange={() => setSearch(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </Search>
 
